125485849d3843941626b84eeae9b2640caf2786
shinnguyen5589 <shinnguyen5589@gmail.com>
2025-06-30 11:38:57 +0700

    First commit
    Add new files

---
+++ Test-SPM-with-kooling-sdk:/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Test-SPM-with-kooling-sdk",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Test-SPM-with-kooling-sdk",
+            targets: ["Test-SPM-with-kooling-sdk"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Test-SPM-with-kooling-sdk"),
+        .testTarget(
+            name: "Test-SPM-with-kooling-sdkTests",
+            dependencies: ["Test-SPM-with-kooling-sdk"]
+        ),
+    ]
+)
---
+++ Test-SPM-with-kooling-sdk:/Tests/Test-SPM-with-kooling-sdkTests/Test_SPM_with_kooling_sdkTests.swift
@@ -0,0 +1,6 @@
+import Testing
+@testable import Test_SPM_with_kooling_sdk
+
+@Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+}
